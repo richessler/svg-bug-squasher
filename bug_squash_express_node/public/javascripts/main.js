d1813c69ee9fc49203532ec4564d1348389d4e60
richessler <richessler@gmail.com>
2014-08-03 02:54:35 -0400

    re-evaluated approach: separated bugs into separate classes which spawn on a timer- have functioning score. TODO: give time to levels, create'home page/game over page' create local storage for space

--- bug_squash_express_node/public/javascripts/main.js
+++ bug_squash_express_node/public/javascripts/main.js
@@ -1,106 +1,110 @@
 enchant();
 
-// function getRandomInt(min, max) {
-//                       return Math.floor(Math.random() * 100)
-//                     };
+window.onload = function() {
+    var game = new Game(700, 700);
+    var level = 1;
+    game.easySpeed = 1;
+    game.medSpeed  = 2;
+    game.hardSpeed = 4;
+    game.touched = true;
+    game.preload('./images/bug1.svg');
 
-var random = Math.floor(Math.random() * 320);
 
+    var Bug = enchant.Class.create(enchant.Sprite, {
+        initialize: function(x, y) {
+            enchant.Sprite.call(this, 85, 85);
+            this.x = x;
+            this.y = y;
+            this.image = game.assets['./images/bug1.svg'];
+            this.frame = 5;
+            game.rootScene.addChild(this);
+        }
+    });
 
-window.onload = function(){
-    var game = new Core(700, 700);
-    game.fps = random;
-    game.preload("./images/bug1.svg");
+    var EasyBug = enchant.Class.create(Bug, {
+        initialize: function(x, y) {
+            Bug.call(this, x, y);
+            this.addEventListener('enterframe', function() {
+                this.x += game.easySpeed;
+                // this.y += game.easySpeed;
+                this.frame = [0, 1, 0, 2][Math.floor(this.age/5) % 4] + 5;
 
-    game.onload = function(){
-        var count = 3;
-        var bug1 = new Sprite(85, 85);
-        var bug2 = new Sprite(85, 85);
-        var bug3 = new Sprite(85, 85);
+                if (this.x === 700){
+                    game.end();
+                }
+            });
+        },
+        ontouchend: function() {
+            game.score += 100;
+            game.rootScene.removeChild(this);
+            // game.rootScene.addChild(scoreLabel);
+        }
+    });
 
-        var startEasyBug = function(){
-            bug1.image = game.assets["./images/bug1.svg"];
-            bug1.x = 0;
-            bug1.y = Math.floor(Math.random() * 250);
-            bug1.frame = 5;
-            game.rootScene.addChild(bug1);
-        };
+    var MedBug = enchant.Class.create(Bug, {
+        initialize: function(x, y) {
+            Bug.call(this, x, y);
+            this.addEventListener('enterframe', function() {
+                this.x += game.medSpeed;
+                // this.y += game.medSpeed;
+                this.frame = [0, 1, 0, 2][Math.floor(this.age/5) % 4] + 5;
+                if (this.x === 700){
+                    game.end();
+                }
+            });
+        },
+        ontouchend: function() {
+            game.score += 100;
+            game.rootScene.removeChild(this);
+        }
+    });
 
-        var startMediumBug = function() {
-            bug2.image = game.assets["./images/bug1.svg"];
-            bug2.x = 0;
-            bug2.y = Math.floor(Math.random() * 250);
-            bug2.frame = 5;
-            game.rootScene.addChild(bug2);
-        };
+    var HardBug = enchant.Class.create(Bug, {
+        initialize: function(x, y) {
+            Bug.call(this, x, y);
+            this.addEventListener('enterframe', function() {
+                this.x += game.hardSpeed;
+                // this.y += game.hardSpeed;
+                this.frame = [0, 1, 0, 2][Math.floor(this.age/5) % 4] + 5;
+                if (this.x === 700){
+                    game.end();
+                }
+            });
+        },
+        ontouchend: function() {
+            game.score += 100;
+            game.rootScene.removeChild(this);
+        }
+    });
 
-        var startHardBug = function() {
-            bug3.image = game.assets["./images/bug1.svg"];
-            bug3.x = 0;
-            bug3.y = Math.floor(Math.random() * 250);
-            bug3.frame = 5;
-            game.rootScene.addChild(bug3);
-        };
+    game.onload = function() {
+        game.rootScene.addEventListener('enterframe', function() {
+            game.score = 0;
+            scoreLabel = new Label("Score: ");
+            scoreLabel.addEventListener('enterframe', function(){
+                this.text = "Score:"+game.score;
+            });
+            scoreLabel.x = 600;
+            scoreLabel.color = "black";
+            scoreLabel.font = "18px 'Helvetica'";
+            game.rootScene.addChild(scoreLabel);
 
-        startEasyBug();
-        startMediumBug();
-        startHardBug();
-
-        bug1.addEventListener("enterframe", function(){
-            this.x += 1;
-            this.frame = this.age % 2 + 6;
-            if (this.x === 700){
-                game.stop();
+            if(this.age % 20 === 0 && level < 3 ){
+                var easyBug = new EasyBug(0, rand(320));
+            // } else if (this.age % 20 === 0 && level > 2 && level < 7 ) {
+            //     var easyBug = new EasyBug(0, rand(320));
+            //     var medBug  = new MedBug(0, rand(320));
+            //     var hardBug = new HardBug(0, rand(320));
+            // } else {
+            //     var easyBug = new EasyBug(0, rand(320));
+            //     var medBug  = new MedBug(0, rand(320));
+            //     var hardBug = new HardBug(0, rand(320));
             }
-        });
 
-        bug2.addEventListener("enterframe", function(){
-            this.x += 2;
-            this.frame = this.age % 2 + 6;
-            if (this.x === 700){
-                game.stop();
-            }
         });
-
-        bug3.addEventListener("enterframe", function(){
-            this.x += 5;
-            this.frame = this.age % 2 + 6;
-            // if (this.x === 700){
-            //     game.stop();
-            // }
-        });
-
-        bug1.addEventListener("touchstart", function(){
-            game.rootScene.removeChild(bug1);
-            for (var i = 0; i <= count; i++) {
-                startEasyBug();
-            }
-            count++;
-        });
-
-        bug2.addEventListener("touchstart", function(){
-            game.rootScene.removeChild(bug2);
-            for (var i = 0; i <= count; i++) {
-                startMediumBug();
-            }
-            count++;
-        });
-
-        bug3.addEventListener("touchstart", function(){
-            game.rootScene.removeChild(bug3);
-            for (var i = 0; i <= count; i++) {
-                startHardBug();
-            }
-            count++;
-        });
-
-        // if (count===2){
-        //         location.reload();
-        //     }
-        var loseGame = function(){
-            if (bug1.x === 320 || bug2.x === 320){
-            }
-        };
     };
     game.start();
 };
+function rand(num){
+    return Math.floor( Math.random() * num);
+}
